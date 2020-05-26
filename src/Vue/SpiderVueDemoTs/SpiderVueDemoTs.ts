105ffe29be1dc60b8bc2f59042f1ca1634918d2f
jerry <398265072@qq.com>
2020-05-26 23:42:04 +0800

    .d.ts .vue config

--- src/Vue/SpiderVueDemoTs/SpiderVueDemoTs.ts
+++ src/Vue/SpiderVueDemoTs/SpiderVueDemoTs.ts
@@ -3,5 +3,5 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class SpiderVueDemoTs extends Vue {
   @Prop()
-  private msg!: string;
+  private msg: string = "Hello SpiderVueDemoTs";
 }
---
+++ src/Vue/SpiderVueDemoTs2/SpiderVueDemoTs2.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<script lang="ts" src="./SpiderVueDemoTs2.ts">
+</script>
