afda3ac8203a2e1a9eab03e3c610969d781be56e
jerry <398265072@qq.com>
2020-06-01 11:18:14 +0800

    vue tab

--- src/Vue/SpiderVueTab/SpiderVueTab.ts
+++ src/Vue/SpiderVueTab/SpiderVueTab.ts
@@ -3,5 +3,18 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class SpiderVueTab extends Vue {
   @Prop()
+  private tabs: {
+    title: string,
+    name: string
+  }[] = [
+      {
+        title: 'Tab1',
+        name: 'Tab1'
+      }, {
+        title: 'Tab2',
+        name: 'Tab2'
+      }
+    ];
+  @Prop()
   private msg: string = "Hello SpiderVueTab";
 }
--- src/Vue/SpiderVueTab/SpiderVueTab.vue
+++ src/Vue/SpiderVueTab/SpiderVueTab.vue
@@ -1,9 +1,6 @@
 <template>
   <el-tabs v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane label="User" name="first">User</el-tab-pane>
-    <el-tab-pane label="Config" name="second">Config</el-tab-pane>
-    <el-tab-pane label="Role" name="third">Role</el-tab-pane>
-    <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+    <el-tab-pane v-for="tab in tabs" :label="tab.title">{{tab.name}}</el-tab-pane>
   </el-tabs>
 </template>
 
